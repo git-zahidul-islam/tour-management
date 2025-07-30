6a74a186e6807b8d3732557812751794fdaa77df
zahidul islam <freelancerzahidf101@gmail.com>
2025-07-30 08:55:23 +0600

    part 2 complete

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,10 +6,15 @@ interface EnvConfig {
     PORT: string,
     DB_URL: string,
     NODE_ENV: "productions" | "development",
+    BCRYPT_SALT_ROUND: string,
+    JWT_ACCESS_EXPIRES: string,
+    JWT_ACCESS_SECRET: string,
+    SUPER_ADMIN_EMAIL: string,
+    SUPER_ADMIN_PASSWORD:string,
 };
 
 const loadEnvVariable = () : EnvConfig => {
-    const requareEnvVariable = ["PORT", "DB_URL", "NODE_ENV"];
+    const requareEnvVariable = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND","JWT_ACCESS_EXPIRES","JWT_ACCESS_SECRET","SUPER_ADMIN_EMAIL","SUPER_ADMIN_PASSWORD"];
 
     requareEnvVariable.forEach(key => {
         if (!process.env[key]) {
@@ -21,6 +26,11 @@ const loadEnvVariable = () : EnvConfig => {
         PORT: process.env.PORT!,
         DB_URL: process.env.DB_URL!,
         NODE_ENV: process.env.NODE_ENV as "productions" | "development",
+        BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string
     };
 
 };
