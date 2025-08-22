4cd7ab6355bff838aba796a9f32c525d36157d45
zahidul islam <freelancerzahidf101@gmail.com>
2025-08-22 11:18:59 +0600

    Implement division and tour management features with CRUD operations, validation, and routing

--- src/app/modules/division/division.model.ts
+++ src/app/modules/division/division.model.ts
@@ -11,4 +11,42 @@ const divisionSchema = new Schema<IDivision>({
     timestamps: true
 })
 
+divisionSchema.pre("save", async function (next) {
+    if (this.isModified(this.name)) {
+        const baseSlug = this.name.toLowerCase().split(" ").join("-")
+        let slug = `${baseSlug}`
+
+        let counter = 0;
+        while (await Division.exists({ slug })) {
+            slug = `${slug}-${counter++}` // dhaka-division-2
+        }
+
+        this.slug = slug;
+    }
+
+    next();
+});
+
+divisionSchema.pre("findOneAndUpdate", async function (next) {
+    const divisiton = this.getUpdate() as Partial<IDivision>;
+
+    if (divisiton.name) {
+        if (divisiton.name) {
+            const baseSlug = divisiton.name.toLowerCase().split(" ").join("-")
+            let slug = `${baseSlug}`
+
+            let counter = 0;
+            while (await Division.exists({ slug })) {
+                slug = `${slug}-${counter++}` // dhaka-division-2
+            }
+
+            divisiton.slug = slug
+        }
+    };
+
+    this.setUpdate(divisiton);
+
+    next();
+})
+
 export const Division = model<IDivision>("Division", divisionSchema)
